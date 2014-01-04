7ebc05efa780e2b84ee185181164df68ca44b183
Paul Wolf <paul.wolf@yewleaf.com>
2014-01-04 11:48:48 +0000

    refactored directories for strgen package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='StringGenerator',
+    version='0.1',
+    packages=['strgen',],
+    license='',
+    long_description=open('README.md').read(),
+)
+
